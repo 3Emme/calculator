e9177b220621df50c6723dc1a09af15e9a0e882a
3Emme <emmettbuentiempo@gmail.com>
2020-08-12 11:16:32 -0700

    further update index.html to include forms for 2 numbers and alter scripts.js to add instead of divdide.
    co-authored-by: Garrett D Brown <gman9mm@live.com>

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,9 @@ function multiply(number1, number2) {
 function divide(number1, number2) {
   return number1 / number2;
 }
+
 const number1 = parseInt(prompt("enter a number:"));
 alert(number1);
 const number2 = parseInt(prompt("enter another number:"));
-const result = divide(number1, number2);
+const result = add(number1, number2);
 alert(result);
