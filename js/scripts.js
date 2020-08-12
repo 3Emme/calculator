f49dd3608e8be21a6ac4088dba8608ddf27ada9f
3Emme <emmettbuentiempo@gmail.com>
2020-08-12 13:57:44 -0700

    add formatting styles to calculator grid via even-grid-col-6 class (adding top and bottom padding). Complete proliferation of multiplication and division functions in scripts.js
    co-authored-by: Garrett D Brown <gman9mm@live.com>

--- js/scripts.js
+++ js/scripts.js
@@ -44,6 +44,30 @@ $(document).ready(function() {
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    $("#output").text(result);
+    $("#output-add").text(result);
+  });
+  
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
+    const result = sub(number1, number2);
+    $("#output-sub").text(result);
+  });
+
+  $("form#mul").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#mul1").val());
+    const number2 = parseInt($("#mul2").val());
+    const result = mul(number1, number2);
+    $("#output-mul").text(result);
+  });
+  
+  $("form#div").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#div1").val());
+    const number2 = parseInt($("#div2").val());
+    const result = div(number1, number2);
+    $("#output-div").text(result);
   });
 });
