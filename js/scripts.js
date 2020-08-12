dc8ee29701e38e3e473dd378ac7785cc3db9368e
3Emme <emmettbuentiempo@gmail.com>
2020-08-12 13:59:20 -0700

    removed commented out code in scripts.js
    co-authored-by: Garrett D Brown <gman9mm@live.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,3 @@
-//function add(number1, number2) {
-//  return number1 + number2;
-//}
-//
-//function subtract(number1, number2) {
-//  return number1 - number2;
-//}
-//
-//function multiply(number1, number2) {
-//  return number1 * number2;
-//}
-//
-//function divide(number1, number2) {
-//  return number1 / number2;
-//}
-//
-//const number1 = parseInt(prompt("enter a number:"));
-//alert(number1);
-//const number2 = parseInt(prompt("enter another number:"));
-//const result = add(number1, number2);
-//alert(result);
-
-// Business logic:
-
 function add(number1, number2) {
   return number1 + number2;
 }
