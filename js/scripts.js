a088cba2ea79f52dab70e8c7e91db7910dfbb4df
3Emme <emmettbuentiempo@gmail.com>
2020-08-12 13:34:10 -0700

    replicate calculator functionality for subtraction, multiplication, and division. Also add grid format for all four calculator segments.
    co-authored-by: Garrett D Brown <gman9mm@live.com>

--- js/scripts.js
+++ js/scripts.js
@@ -26,15 +26,15 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-function subtract(number1, number2) {
+function sub(number1, number2) {
   return number1 - number2;
 }
 
-function multiply(number1, number2) {
+function mul(number1, number2) {
   return number1 * number2;
 };
 
-function divide(number1, number2) {
+function div(number1, number2) {
   return number1 / number2;
 };
 
